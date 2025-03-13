2609d53f805f157654a92bc578485a13c95ecdd2
“[firstname <dafne.faviola.luna@gmail.com>
2025-03-13 18:52:39 -0400

    took out kendo for layout, added bootstrap, uploaded images

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,29 +1,30 @@
-// import { useState } from 'react'
-import './App.css'
-import { GridLayout, GridLayoutItem } from '@progress/kendo-react-layout';
-// import Navbar from './comps/navbar'
+import { Container, Row, Col} from 'react-bootstrap'
 
+import './App.css'
+
+// note: these classes are from the utilities in Bootstrap documentation to override the default stylesheet
+// note: md is the width of the column
 function App() {
 
   return (
-    <>
-      {/* <Navbar /> */}
-      <GridLayout  gap={{rows: 2, cols: 3}}>
-        <GridLayoutItem className='box'>
-        <h1>Dafne Luna</h1>
-        </GridLayoutItem>
-        <GridLayoutItem>
-          <h2>EdBox</h2>
-        </GridLayoutItem>
-        <GridLayoutItem className='box'>
-          <h2>DevBox</h2>
-        </GridLayoutItem>
-        <GridLayoutItem>
-          <h1>sidebar</h1>
-        </GridLayoutItem>
-      
-      </GridLayout>
-    </>
+    <Container fluid className="p-0 h-100">
+      <Row className="h-100 g-0">
+        <Col xs={12} md={6} className="d-flex align-items-center justify-content-center box1">
+          Dafne Luna
+        </Col>
+        <Col xs={12} md={4} className="d-flex flex-column">
+          <div className="edbox flex-grow-1 d-flex align-items-center justify-content-center">
+            EdBox
+          </div>
+          <div className="devbox flex-grow-1 d-flex align-items-center justify-content-center">
+            DevBox
+          </div>
+        </Col>
+        <Col xs={12} md={2} className="d-flex align-items-center justify-content-center sidebox">
+          Sidebar
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
